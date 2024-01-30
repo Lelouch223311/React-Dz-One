cb449a5f4aa273fb542c2716d10eb89860ae9d50
vanessaparfeni <vanessaparfeni123>
2024-01-30 15:58:33 +0300

    Добавил комментарии к App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+// Импортируем всякое
 import './App.css';
 import { useState, useEffect } from 'react';
 import { Button } from '@mui/material'
@@ -7,18 +7,19 @@ import { Generator } from './components/Genik';
 function App() {
 
 
-  let [number, setNumber] = useState(0)
+  let [number, setNumber] = useState(0) // number это наше число (изначально: 0)
+  // setNumber это наша "функция" которая обновляет значение number
   const clickThis = () =>{
 
-    setNumber(number = Math.floor(Math.random() * 530))
+    setNumber(number = Math.floor(Math.random() * 530)) // Само обновления значения,на рандомное число/цифру.
 
   }
 
   return (
     <div className="App">
       <header className="App-header">
-      <Generator number={number}/> 
-      <Button onClick={clickThis}>Показать рандом число/цифру</Button>
+      <Generator number={number}/>  {/*  Generator это  компонент который принимает props number(Genik.jsx) */}
+      <Button onClick={clickThis}>Показать рандом число/цифру</Button> {/* Сама кнопка через MUI */}
       </header>
     </div>
   );
